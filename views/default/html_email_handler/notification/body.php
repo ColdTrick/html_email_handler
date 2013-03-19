e9c287a7b90357451fb3eb4ddc0ea1de9466d6d7
Jerome Bakker <jeabakker@coldtrick.com>
2013-03-19 14:56:49 +0100

    fixed: mail message language now set correctly

--- views/default/html_email_handler/notification/body.php
+++ views/default/html_email_handler/notification/body.php
@@ -1,10 +1,11 @@
 <?php 
 	$title = $vars["title"];
 	$message = nl2br($vars["message"]);
+	$language = get_current_language();
 	
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $language; ?>" lang="<?php echo $language; ?>">
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 		<base target="_blank" />
