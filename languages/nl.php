f685677576b117e591d3e6b2005fbcd5b70811a7
Jerome Bakker <jeabakker@coldtrick.com>
2014-07-17 09:24:21 +0200

    dutch translation

---
+++ languages/nl.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * This file was created by Translation Editor v2.0
+ * On 2014-07-17 09:23
+ */
+
+$language = array (
+  'html_email_handler' => 'HTML E-mail handler',
+  'html_email_handler:theme_preview:menu' => 'HTML notificatie',
+  'html_email_handler:settings:notifications:description' => 'Als je deze optie inschakelt zullen alle notificaties naar de gebruikers worden verzonden in HTML formaat.',
+  'html_email_handler:settings:notifications' => 'Gebruik als standaard e-mail notificatie methode',
+  'html_email_handler:settings:notifications:subtext' => 'Dit zal alle e-mail berichten verzenden als HTML mails',
+  'html_email_handler:settings:sendmail_options' => 'Additionele parameters voor sendmail (optioneel)',
+  'html_email_handler:settings:sendmail_options:description' => 'Hier kun je additionele parameters instellen voor sendmail, bijvoorbeeld -f % (om ervoor te zorgen dat e-mails minder snel worden gemarkeerd als spam)',
+  'html_email_handler:notification:footer:settings' => 'Configureer je notificatie instellingen %shier%s',
+);
+add_translation("nl", $language);
