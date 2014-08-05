bdf3deed61ef780bae5535e0b1c6d04f7874a8d0
vfelix <vfelix@GTS-W2K3-FB.SWG.CDC.ROOT>
2014-08-05 10:17:46 -0400

    save file sanity corrections

--- actions/save.php
+++ actions/save.php
@@ -1,6 +1,5 @@
 <?php
 /**
- * Intercept saving function
  * @uses array $_REQUEST['params']    A set of key/value pairs to save to the ElggPlugin entity
  * @uses int   $_REQUEST['plugin_id'] The ID of the plugin
  */
@@ -14,20 +13,20 @@ if (!($plugin instanceof ElggPlugin)) {
 $result = false;
 foreach ($params as $k => $v) {
     switch ($k) {
-        case 'smtp_pass':
-            $result = $plugin->setSetting($k, setuppassword($v));
-            break;
-        default:
-            $result = $plugin->setSetting($k, $v); //where no changes are required
-            break;
+    case 'smtp_pass':
+    $result = $plugin->setSetting($k, setuppassword($v));
+    break;
+    default:
+    $result = $plugin->setSetting($k, $v); //where no changes are required
+    break;
     }
 		
-		if (!$result) {
-			register_error(elgg_echo('plugins:settings:save:fail', array($plugin_name)));
-			forward(REFERER);
-			exit;
-		}
-	};
+    if (!$result) {
+            register_error(elgg_echo('plugins:settings:save:fail', array($plugin_name)));
+            forward(REFERER);
+            exit;
+    }
+};
 system_message(elgg_echo('plugins:settings:save:ok', array($plugin_name)));
 forward(REFERER);
 
@@ -37,6 +36,5 @@ forward(REFERER);
  * @return string   The encrypted string
  */
 function setuppassword($pstring){
-    if(trim($pstring) != '' || $pstring != 'xxxxxxxx') return base64_encode($pstring);
+    if (trim($pstring) != '' || $pstring != '********') return base64_encode($pstring);
 }
-?>
