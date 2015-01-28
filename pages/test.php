ab0bd77f91f2079a2c3ca8ca9a9d2fe2cd0a1515
Jerome Bakker <jeabakker@coldtrick.com>
2015-01-28 13:25:08 +0100

    added: option to send the test page to email changed: test page uses inline CSS, normalized urls and embedded images

--- pages/test.php
+++ pages/test.php
@@ -3,13 +3,32 @@
  * A test page for theme developer to view the layout of an email notification
  */
 
-admin_gatekeeper();
+elgg_admin_gatekeeper();
 
-$title = elgg_echo("useradd:subject");
-$message = elgg_echo("useradd:body");
+$user = elgg_get_logged_in_user_entity();
 
-echo elgg_view("html_email_handler/notification/body", array(
-	"subject" => $title,
-	"body" => $message,
-	"recipient" => elgg_get_logged_in_user_entity()
+$subject = elgg_echo("useradd:subject");
+$plain_message = elgg_echo("useradd:body");
+
+$html_message = elgg_view("html_email_handler/notification/body", array(
+	"subject" => $subject,
+	"body" => $plain_message,
+	"recipient" => $user
 ));
+
+$html_message = html_email_handler_css_inliner($html_message);
+$html_message_ext = html_email_handler_normalize_urls($html_message);
+$html_message_ext = html_email_handler_embed_images($html_message_ext);
+
+echo $html_message_ext;
+
+if (get_input("mail")) {
+	$options = array(
+		"to" => $user->email,
+		"subject" => $subject,
+		"plaintext_message" => $plain_message,
+		"html_message" => $html_message
+	);
+	
+	html_email_handler_send_email($options);
+}
