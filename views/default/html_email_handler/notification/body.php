f57735c7bbf517548cdda55e2ea1255a03d88142
Jerome Bakker <jeabakker@coldtrick.com>
2016-01-20 10:45:35 +0100

    fixed: powered by Elgg logo location

--- views/default/html_email_handler/notification/body.php
+++ views/default/html_email_handler/notification/body.php
@@ -1,5 +1,7 @@
 <?php
 
+use Elgg\Filesystem\Directory;
+
 $subject = elgg_extract("subject", $vars);
 $message = nl2br(elgg_extract("body", $vars));
 $language = elgg_extract("language", $vars, get_current_language());
@@ -7,7 +9,10 @@ $recipient = elgg_extract("recipient", $vars);
 
 $site = elgg_get_site_entity();
 $site_url = elgg_get_site_url();
-	
+
+$isElggAtRoot = Elgg\Application::elggDir()->getPath() === Directory\Local::root()->getPath();
+$elggSubdir = $isElggAtRoot ? '' : 'vendor/elgg/elgg/';
+
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $language; ?>" lang="<?php echo $language; ?>">
@@ -17,19 +22,23 @@ $site_url = elgg_get_site_url();
 		
 		<?php
 			if (!empty($subject)) {
-				echo "<title>" . $subject . "</title>";
+				echo elgg_format_element('title', [], $subject);
 			}
 		?>
 	</head>
 	<body>
 		<style type="text/css">
-			<?php echo elgg_view("css/html_email_handler/notification"); ?>
+			<?php echo elgg_view('css/html_email_handler/notification'); ?>
 		</style>
 	
 		<div id="notification_container">
 			<div id="notification_header">
 				<?php
-					echo elgg_view("output/url", array("href" => $site_url, "text" => $site->name, "is_trusted" => true));
+					echo elgg_view('output/url', [
+						'href' => $site_url,
+						'text' => $site->name,
+						'is_trusted' => true,
+					]);
 				?>
 			</div>
 			<div id="notification_wrapper">
@@ -46,16 +55,19 @@ $site_url = elgg_get_site_url();
 			
 			<div id="notification_footer">
 				<a href="http://www.elgg.org/" id="notification_footer_logo">
-					<img src="<?php echo $site_url; ?>_graphics/powered_by_elgg_badge_drk_bckgnd.gif" />
+					<img src="<?php echo $site_url . $elggSubdir; ?>_graphics/powered_by_elgg_badge_drk_bckgnd.gif" />
 				</a>
 				
 				<?php
 					if (!empty($recipient) && ($recipient instanceof ElggUser)) {
-						$settings_url = $site_url . "settings/user/" . $recipient->username;
-						if (elgg_is_active_plugin("notifications")) {
-							$settings_url = $site_url . "notifications/personal/" . $recipient->username;
+						$settings_url = "{$site_url}settings/user/{$recipient->username}";
+						if (elgg_is_active_plugin('notifications')) {
+							$settings_url = "{$site_url}notifications/personal/{$recipient->username}";
 						}
-						echo elgg_echo("html_email_handler:notification:footer:settings", array("<a href='" . $settings_url . "'>", "</a>"));
+						echo elgg_echo('html_email_handler:notification:footer:settings', [
+							"<a href='{$settings_url}'>",
+							'</a>',
+						]);
 					}
 				?>
 				<div class="clearfloat"></div>
