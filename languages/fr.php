9d0e9201d96cfc4bc516b45a7cb84700309ccb28
Florian DANIEL ~ Facyla <i.daniel.florian@gmail.com>
2016-12-14 16:57:34 +0100

    Add missing translation keys + update description translation

--- languages/fr.php
+++ languages/fr.php
@@ -6,9 +6,17 @@ $french = array(
 	'html_email_handler:theme_preview:menu' => "Notification HTML",
 	
 	// settings
-	'html_email_handler:settings:notifications:description' => "Lorsque vous activez cette option, toutes les notifications envoyées aux membres du site seront au format HTML (au lieu du texte brut).",
+	'html_email_handler:settings:notifications:description' => "Lorsque vous activez cette option, toutes les notifications envoyées aux membres du site seront au format HTML et text. Une version alternative en texte seul est également envoyée afin de permettre aux utilisateurs ayant choisi d'afficher seulement la version texte de leurs emails.",
 	'html_email_handler:settings:notifications' => "Utiliser comme gestionnaire d'email de notification par défaut",
 	'html_email_handler:settings:notifications:subtext' => "Ceci va envoyer tous les mails sortant au format HTML",
+	'html_email_handler:settings:limit_subject' => "Limiter la longueur maximale du sujet des emails",
+	'html_email_handler:settings:limit_subject:subtext' => "Activer cette option si les membres se plaignent de sujets d'emails illisibles (principalement des utilisateurs d'Outlook). Ceci va potentiellement faire perdre quelques informations dans le sujet.",
+	'html_email_handler:settings:embed_images' => "Intégrer les images dans les emails",
+	'html_email_handler:settings:embed_images:base64' => "Encodage en Base64",
+	'html_email_handler:settings:embed_images:attach' => "Pièces jointes",
+	'html_email_handler:settings:embed_images:subtext' => "Si activé, toutes les images seront intégrées dans les emails. Tous les clients de messagerie ne supportent pas les différentes options, veuillez tester l'option choisie.",
+	'html_email_handler:settings:proxy_host' => "Hôte du proxy pour intégrer les images",
+	'html_email_handler:settings:proxy_port' => "Numéro de port du proxy pour intégrer des images",
 	
 	'html_email_handler:settings:sendmail_options' => "Paramètres additionnels pour sendmail (optionnel)",
 	'html_email_handler:settings:sendmail_options:description' => "Vous pouvez configurer ici des paramètres additionnels lorsque vous utilisez sendmail, par exemple -f %s (pour mieux éviter que les mails soient marqués comme spam)",
