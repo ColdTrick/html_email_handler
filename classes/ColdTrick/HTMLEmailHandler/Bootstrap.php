957d9e9c0c4e4248dd2f683e3b2dded693a6fcd3
Jerome Bakker <jeabakker@coldtrick.com>
2019-01-31 15:01:12 +0100

    chore: apply sendmail params

--- classes/ColdTrick/HTMLEmailHandler/Bootstrap.php
+++ classes/ColdTrick/HTMLEmailHandler/Bootstrap.php
@@ -8,13 +8,25 @@ class Bootstrap extends DefaultPluginBootstrap {
 	
 	/**
 	 * {@inheritDoc}
+	 * @see \Elgg\DefaultPluginBootstrap::boot()
+	 */
+	public function boot() {
+		$this->setSendmailParams();
+	}
+	
+	/**
+	 * {@inheritDoc}
 	 * @see \Elgg\DefaultPluginBootstrap::init()
 	 */
 	public function init() {
-		
 		$this->registerHooks();
 	}
 	
+	/**
+	 * Register plugin hooks
+	 *
+	 * @return void
+	 */
 	protected function registerHooks() {
 		$hooks = $this->elgg()->hooks;
 		
@@ -22,5 +34,26 @@ class Bootstrap extends DefaultPluginBootstrap {
 		$hooks->registerHandler('prepare', 'system:email', __NAMESPACE__ . '\Email::limitSubjectLength');
 		$hooks->registerHandler('register', 'menu:theme_sandbox', __NAMESPACE__ . '\ThemeSandbox::menu');
 		$hooks->registerHandler('zend:message', 'system:email', __NAMESPACE__ . '\Email::makeHtmlMail');
+	}
+	
+	/**
+	 * Apply additional sendmail params to mailer
+	 *
+	 * @return void
+	 */
+	protected function setSendmailParams() {
+		
+		$params = $this->plugin->getSetting('sendmail_options');
+		if (empty($params)) {
+			return;
+		}
+		
+		$mailer = _elgg_services()->mailer;
+		if (!$mailer instanceof \Zend\Mail\Transport\Sendmail) {
+			// using different mail handler
+			return;
+		}
+		
+		$mailer->setParameters($params);
 	}
 }
