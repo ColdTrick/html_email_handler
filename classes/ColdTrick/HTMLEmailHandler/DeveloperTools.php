1b5c0199186981f46afc16edc81f5eb99fe892c7
Jerome Bakker <jeabakker@coldtrick.com>
2019-02-01 13:58:05 +0100

    chore: improved output blocking

--- classes/ColdTrick/HTMLEmailHandler/DeveloperTools.php
+++ classes/ColdTrick/HTMLEmailHandler/DeveloperTools.php
@@ -2,15 +2,46 @@
 
 namespace ColdTrick\HTMLEmailHandler;
 
+use Elgg\ViewsService;
+
 class DeveloperTools {
 	
+	/**
+	 * Prevent the developer log output
+	 *
+	 * @param \Elgg\Hook $hook 'view_vars', 'html_email_handler/notification/body'
+	 *
+	 * @return void
+	 */
 	public static function preventLogOutput(\Elgg\Hook $hook) {
 		// prevent developer tools output
-		elgg_register_plugin_hook_handler('view_vars', 'developers/log', '\Elgg\Values::preventViewOutput');
+		elgg_register_plugin_hook_handler('view_vars', 'developers/log', self::class . '::blockOutput');
 	}
 	
+	/**
+	 * Re-enable developer log output
+	 *
+	 * @param \Elgg\Hook $hook 'view', 'html_email_handler/notification/body'
+	 *
+	 * @return void
+	 */
 	public static function reenableLogOutput(\Elgg\Hook $hook) {
 		// re-enable developer tools output
-		elgg_unregister_plugin_hook_handler('view_vars', 'developers/log', '\Elgg\Values::preventViewOutput');
+		elgg_unregister_plugin_hook_handler('view_vars', 'developers/log', self::class . '::blockOutput');
+	}
+	
+	/**
+	 * Prevent view output
+	 *
+	 * @param \Elgg\Hook $hook 'view_vars', 'developers/log'
+	 *
+	 * @return array
+	 */
+	public static function blockOutput(\Elgg\Hook $hook) {
+		
+		$vars = $hook->getValue();
+		$vars[ViewsService::OUTPUT_KEY] = '';
+		
+		return $vars;
 	}
 }
